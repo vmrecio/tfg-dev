1aa327f5d13f57a0158aec8a0ef6aba4e96d79a4
vmrecio <victorrecio@gmail.com>
2025-09-05 01:47:48 +0200

    feat: add last_name field with migration; update factory and seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,56 +15,63 @@ class UserSeeder extends Seeder
     public function run(): void
     {
        $admin = User::create([
-            'name' => 'Administrador_TEST',
-            'email' => 'admin@example.com',
+            'name' => 'Víctor',
+            'last_name' => 'Recio',
+            'email' => 'victor@example.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
         $admin->roles()->attach(Role::ADMIN);
 
         $couple1 = User::create([
-            'name' => 'Miembro_Pareja_1_TEST',
-            'email' => 'couple@example.com',
+            'name' => 'Félix',
+            'last_name' => 'Blanco',
+            'email' => 'felix@example.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
         $couple1->roles()->attach(Role::USER);
 
         $couple2 = User::create([
-            'name' => 'Miembro_Pareja_2_TEST',
-            'email' => 'couple2@example.com',
+            'name' => 'Cristina',
+            'last_name' => 'Cantos',
+            'email' => 'cristina@example.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
         $couple2->roles()->attach(Role::USER);
 
         $vendor1 = User::create([
-            'name' => 'Proveedor_1_TEST',
-            'email' => 'vendor@example.com',
+            'name' => 'Víctor',
+            'last_name' => 'Morante',
+            'email' => 'morante@example.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
         $vendor1->roles()->attach(Role::USER);
 
         $vendor2 = User::create([
-            'name' => 'Proveedor_2_TEST',
-            'email' => 'vendor2@example.com',
+            'name' => 'Minerva',
+            'last_name' => 'Mateos',
+            'email' => 'minerva@example.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
         $vendor2->roles()->attach(Role::USER);
 
         $guest1 = User::create([
-            'name' => 'Invitado_1_TEST',
-            'email' => 'guest@example.com',
+            'name' => 'David',
+            'last_name' => 'Casarrubio',
+            'email' => 'deivid@example.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
         $guest1->roles()->attach(Role::USER);
 
         $guest2 = User::create([
-            'name' => 'Invitado_2_TEST',
-            'email' => 'guest2@example.com',
+            'name' => 'Ángela',
+            'last_name' => 'García',
+            'email' => 'angela@example.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
