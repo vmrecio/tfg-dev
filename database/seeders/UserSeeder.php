316eecf819a2f2ece1efc017022aacf83577102f
vmrecio <victorrecio@gmail.com>
2025-09-04 18:33:15 +0200

    feat: add UserSeeder to populate initial users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       $admin = User::create([
+            'name' => 'Administrador_TEST',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+        $admin->roles()->attach(Role::ADMIN);
+
+        $couple1 = User::create([
+            'name' => 'Miembro_Pareja_1_TEST',
+            'email' => 'couple@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+        $couple1->roles()->attach(Role::COUPLE);
+
+        $couple2 = User::create([
+            'name' => 'Miembro_Pareja_2_TEST',
+            'email' => 'couple2@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+        $couple2->roles()->attach(Role::COUPLE);
+
+        $vendor1 = User::create([
+            'name' => 'Proveedor_1_TEST',
+            'email' => 'vendor@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+        $vendor1->roles()->attach(Role::VENDOR);
+
+        $vendor2 = User::create([
+            'name' => 'Proveedor_2_TEST',
+            'email' => 'vendor2@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+        $vendor2->roles()->attach(Role::VENDOR);
+
+        $guest1 = User::create([
+            'name' => 'Invitado_1_TEST',
+            'email' => 'guest@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+        $guest1->roles()->attach(Role::GUEST);
+
+        $guest2 = User::create([
+            'name' => 'Invitado_2_TEST',
+            'email' => 'guest2@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+        $guest2->roles()->attach(Role::GUEST);
+    }
+}
